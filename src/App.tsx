880481076b11fe3ba6aa2fe581f69bee882a59c1
macbook <macbook@MacBook-Pro-cua-macbook.local>
2023-04-24 14:28:32 +0700

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-     <h1 className='text-red-500 text-2xl'>helo word</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          
+        </Route>
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
