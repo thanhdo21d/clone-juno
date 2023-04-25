bb60f0ffd19977aeb909a8c752fbd04c6ade437e
teatw001 <duydhph26793@fpt.edu.vn>
2023-04-25 21:07:55 +0700

    slider image

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Banner1 from "../../assets/image/banner-3.jpeg"
-import Banner2 from "../../assets/image/banner-2.webp"
-import { Outlet } from 'react-router-dom'
-import HangMoi from '../HangMoi/HangMoi'
+import React from "react";
+import Banner1 from "../../assets/image/banner-3.jpeg";
+import Banner2 from "../../assets/image/banner-2.webp";
+import { Outlet } from "react-router-dom";
+import HangMoi from "../HangMoi/HangMoi";
+import Slider from "../slider/Slider";
 
 const Banner = () => {
   return (
-      <div className=''>
-          <img className='w-full h-screen' src={Banner1} />
-          <HangMoi/>
+    <div className="">
+      <Slider />
+      <HangMoi />
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+const Slider = () => {
+  const slides = [
+    {
+      url: "https://file.hstatic.net/1000003969/file/kv-take-me-to-summer-1920x870_8e1a29d7b61e428fb389fcf4f5619416.jpg",
+    },
+    {
+      url: "https://file.hstatic.net/1000003969/file/kv-ja-1920x870_2243b13ddea6494dac1beed3535999b8.jpg",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState<number | undefined>();
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex: any) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      nextSlide();
+    }, 5000);
+    setIntervalId(id);
+    return () => clearInterval(id);
+  }, [currentIndex]);
+
+  return (
+    <div className="max-w-full h-[780px] w-full m-auto pb-16 over overflow-hidden  group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+      ></div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
